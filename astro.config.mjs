c5d80be624e7e73c1505992c2765b37b021a1a4a
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2026-02-27 01:27:33 -0500

    feat: migrate to Cloudflare Workers adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: cloudflare()
 });
