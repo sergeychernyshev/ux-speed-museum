86773372bad46eb46a21d60abf61d1682238cb6b
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2026-02-27 21:51:45 -0500

    feat: switch to static generation, remove cookies, and fix theme flickering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-  }),
+  output: 'static',
 });
